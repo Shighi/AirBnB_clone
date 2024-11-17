a1ffb7eb7a9069013300bb4de09aeabb75e36867
Shighi <daisyshighi@gmail.com>
2024-11-17 11:33:16 -0800

    2

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,8 @@
 
 import uuid
 from datetime import datetime
-from models import storage
-
 
 class BaseModel:
-
     """This is the class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
@@ -17,7 +14,6 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -32,23 +28,23 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            # Move import inside method
+            from models import storage  # Delayed import to avoid circular import
             storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
-
         self.updated_at = datetime.now()
+        # Move import inside method
+        from models import storage  # Delayed import to avoid circular import
         storage.save()
 
     def to_dict(self):
         """This returns a dictionary containing all keys/values of __dict__"""
-
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
